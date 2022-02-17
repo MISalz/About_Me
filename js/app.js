52dedb42ceb4f2576fb2686a2a63cce878d033c5
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-02-16 23:12:41 -0800

    uncomment previous questions

--- js/app.js
+++ js/app.js
@@ -1,49 +1,49 @@
 'use strict';
 
-// console.log('greetings user');
-// let user = prompt('Thank you for visiting my page, whats your name?');
-// console.log('hi ' + user)
+console.log('greetings user');
+let user = prompt('Thank you for visiting my page, whats your name?');
+console.log('hi ' + user)
 
-// let q1 = prompt('do you know me?').toLowerCase;
+let q1 = prompt('do you know me?').toLowerCase;
 
-// if (q1 === 'yes' || q1 === 'y') {
-//   alert('Great! but I bet you dont know these little facts about me?');
-// } else if (q1 === 'no' || q1 === 'n') {
-//   alert('No? Well heres a few little fun facts about me.')
-// }
-// // console.log (user+' I asked if you knew me and your answer was '+q1)
+if (q1 === 'yes' || q1 === 'y') {
+  alert('Great! but I bet you dont know these little facts about me?');
+} else if (q1 === 'no' || q1 === 'n') {
+  alert('No? Well heres a few little fun facts about me.')
+}
+// console.log (user+' I asked if you knew me and your answer was '+q1)
 
-// let q2 = prompt('Did you know this is my second try at Codefellows 201 course?').toLowerCase;
+let q2 = prompt('Did you know this is my second try at Codefellows 201 course?').toLowerCase;
 
-// if (q2 === 'yes' || q2 === 'y') {
-//   alert(`${user}  Im so glad were in this class together and hope that we get to know each other better.`);
-// } else if (q2 === 'no' || q2 === 'n') {
-//   alert(`${user}  Its been two years since I took my first course and Im nervous because I hope that its truly like riding a bicicle.`)
-// }
-// // console.log (user+' Im so glad we are taking this course together ')
+if (q2 === 'yes' || q2 === 'y') {
+  alert(`${user}  Im so glad were in this class together and hope that we get to know each other better.`);
+} else if (q2 === 'no' || q2 === 'n') {
+  alert(`${user}  Its been two years since I took my first course and Im nervous because I hope that its truly like riding a bicicle.`)
+}
+// console.log (user+' Im so glad we are taking this course together ')
 
-// let q3 = prompt('do you think I had prior coding or programming experience prior to attending codefellows?').toLowerCase;
-// if (q3 === 'yes' || q3 === 'y') {
-//   alert(`${user}  Yes, actually I did. I had taken some courses during college and even self taught myself a little of HTML during the myspace days.`);
-// } else if (q3 === 'no' || q3 === 'n') {
-//   alert(`${user}  Its been so long since I took CIS course in college, but I did have prior experience with programming languages prior to attending CF`)
-// }
-// // console.log (user+' now you know a bit more about me and my journey to Codefellows.')
+let q3 = prompt('do you think I had prior coding or programming experience prior to attending codefellows?').toLowerCase;
+if (q3 === 'yes' || q3 === 'y') {
+  alert(`${user}  Yes, actually I did. I had taken some courses during college and even self taught myself a little of HTML during the myspace days.`);
+} else if (q3 === 'no' || q3 === 'n') {
+  alert(`${user}  Its been so long since I took CIS course in college, but I did have prior experience with programming languages prior to attending CF`)
+}
+// console.log (user+' now you know a bit more about me and my journey to Codefellows.')
 
-// let q4 = prompt('Am I currently employed').toLowerCase;
-// if (q4 === 'yes' || q4 === 'y') {
-//   alert(`${user}  Actually not right now. I am taking a break to focus on school`);
-// } else if (q4 === 'no' || q4 === 'n') {
-//   alert(`${user}  you are correct, currently taking a break to attend this course`)
-// }
-// // console.log (user+' now you know a bit more about me and my journey to Codefellows.')
-// let q5 = prompt('Do I know what im doing this time around?').toLowerCase;
-// if (q5 === 'yes' || q5 === 'y') {
-//   alert(`${user}  not really, I remember some of the lessons but it has not hit me yet where I feel like an expert.`);
-// } else if (q5 === 'no' || q5 === 'n') {
-//   alert(`${user}  you are correct, I am currently like everyone else in the class right now learning from the beginning.`)
-// }
-// // console.log (user+' now you know a bit more about me and my journey to Codefellows.')
+let q4 = prompt('Am I currently employed').toLowerCase;
+if (q4 === 'yes' || q4 === 'y') {
+  alert(`${user}  Actually not right now. I am taking a break to focus on school`);
+} else if (q4 === 'no' || q4 === 'n') {
+  alert(`${user}  you are correct, currently taking a break to attend this course`)
+}
+// console.log (user+' now you know a bit more about me and my journey to Codefellows.')
+let q5 = prompt('Do I know what im doing this time around?').toLowerCase;
+if (q5 === 'yes' || q5 === 'y') {
+  alert(`${user}  not really, I remember some of the lessons but it has not hit me yet where I feel like an expert.`);
+} else if (q5 === 'no' || q5 === 'n') {
+  alert(`${user}  you are correct, I am currently like everyone else in the class right now learning from the beginning.`)
+}
+// console.log (user+' now you know a bit more about me and my journey to Codefellows.')
 //------------------
 /* Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
 Indicates through an alert if the guess is “too high” or “too low”.
